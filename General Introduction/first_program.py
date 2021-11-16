11cea3617a868c1dba0723bbbd6b7d800c84de68
Pedro Tambriz <pedro.tambriz.ag@gmail.com>
2021-11-16 17:41:43 -0600

    Adding comment to first_program.py

--- General Introduction/first_program.py
+++ General Introduction/first_program.py
@@ -1,1 +1,7 @@
+# ---------------------------------------------------
+# This demo program shows off how elegant Python is!
+# Written by Joe Soap, December 2010. 
+# Anyone may freely copy or modify this program.  
+# ----------------------------------------------------
+
 print("Hello, World!")
